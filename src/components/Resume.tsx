d3d6734ddd971dfafb2339702e63806dad3751ee
Kreoniz <kreoniz@mail.ru>
2024-10-03 09:59:22 +0600

    feat: finish resume layout

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -12,14 +12,63 @@ export function Resume({
   education: IEducationItem[];
 }) {
   return (
-    <div>
-      Resume:
-      <h1>{title}</h1>
-      <div>
-        {generalInfo.name} {generalInfo.email} {generalInfo.phone}
+    <div className="w-full h-full">
+      <h1 className="font-bold text-2xl text-center mb-2">{title}</h1>
+
+      <div className="w-full h-full px-8 py-4 bg-white shadow-lg flex flex-col gap-2">
+        <div className="text-lg flex justify-center gap-2">
+          <div>{generalInfo.name}</div>
+          <div>{generalInfo.email}</div>
+          <div>{generalInfo.phone}</div>
+        </div>
+
+        <div className="border-b" />
+
+        <div>
+          <h2 className="text-xl font-bold mb-2">Experience:</h2>
+
+          <div className="flex flex-col gap-2">
+            {experience.map((item) => (
+              <div>
+                <div>
+                  <div className="leading-4 font-bold text-lg">
+                    {item.companyName}
+                  </div>
+                  <div className="text-sm font-light">
+                    {item.startDate} - {item.endDate}
+                  </div>
+                </div>
+
+                <div>{item.position}</div>
+                <p className="text-sm">{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="border-b" />
+
+        <div>
+          <h2 className="text-xl font-bold mb-2">Education:</h2>
+
+          <div className="flex flex-col gap-2">
+            {education.map((item) => (
+              <div>
+                <div>
+                  <div className="leading-4 font-bold text-lg">
+                    {item.schoolName}
+                  </div>
+                  <div className="text-sm font-light">
+                    {item.startDate} - {item.endDate}
+                  </div>
+                </div>
+
+                <div>{item.title}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div>{experience.map((item) => item.companyName)}</div>
-      <div>{education.map((item) => item.schoolName)}</div>
     </div>
   );
 }
