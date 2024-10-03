c4aaecd924067b0216dadf20b33a75d3bcf6066c
Kreoniz <kreoniz@mail.ru>
2024-10-03 12:36:03 +0600

    fix: rename svg jsx attributes

--- src/icons/plus.tsx
+++ src/icons/plus.tsx
@@ -9,8 +9,8 @@ export function PlusIcon() {
       className="size-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
       />
     </svg>
